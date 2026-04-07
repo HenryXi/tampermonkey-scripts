60819a204dd4f65a416f6e6ad84e41051a6073fd
HenryXi <xxy668@foxmail.com>
2026-04-07 22:23:37 +0800

    chore: 更新版本号至1.5.1

--- scripts/bilibili-custom-recommendations.user.js
+++ scripts/bilibili-custom-recommendations.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         B站自定义推荐视频
 // @namespace    http://tampermonkey.net/
-// @version      1.5.0
+// @version      1.5.1
 // @description  在B站视频播放页右侧推荐区域添加指定UP主的视频
 // @author       You
 // @match        https://www.bilibili.com/video/*
